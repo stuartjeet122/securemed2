b0e90ba8e247715708848c01e9c6985fdccfb08f
Deepvesh <dhurjoon@umail.utm.ac.mu>
2024-08-05 23:15:45 +0400

    ouii

--- src/Filepath/Pharmacy/pharmacy.jsx
+++ src/Filepath/Pharmacy/pharmacy.jsx
@@ -1,32 +1,10 @@
-import REACT, { useState }  from 'react';
-import ReusableButton from '../../components/Button/ReusableButton';
-import ReusableInput from '../../components/Input/ReusableInput';
+import REACT from 'react';
 
 function Pharmacy(){
-    const [inputValue, setInputValue] = useState('');
-
-    const handleInputChange = (event) => {
-      setInputValue(event.target.value);
-    };
-    const handleClick = () => {
-        alert('Button clicked!');
-      };
-
     return (
         <div>
-        <h1>Welcome to My App</h1>
-        <ReusableInput
-          id="email"
-          type="text"
-          placeholder="Enter text"
-          value={inputValue}
-          onChange={handleInputChange}
-          label="Email Address"
-          className="mt-1"
-        />
-        <ReusableButton title="Hover Me" onClick={handleClick} />
-      </div>
-    
+            <h1>Pharmacy Page</h1>
+        </div>
     );
 }
 
